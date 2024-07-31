ff0efbb8bfad5d918ee023518777f364f7b3d647
sandyr <sandyr@ebi.ac.uk>
2024-07-31 18:49:16 +0100

    tedious workaround for buggy prefect/pytest caplog capture

--- workflows/tests/test_simple_example_flow.py
+++ workflows/tests/test_simple_example_flow.py
@@ -1,10 +1,13 @@
+import logging
+
 import pytest
 
 from workflows.flows.simple_example import github_stars
+from workflows.prefect_utils.testing_utils import run_flow_and_capture_logs
 
 
 @pytest.mark.django_db
-def test_prefect_simple_example_flow(prefect_harness, httpx_mock, caplog):
+def test_prefect_simple_example_flow(prefect_harness, httpx_mock):
     httpx_mock.add_response(
         url="https://api.github.com/repos/EBI-Metagenomics/emg-viral-pipeline",
         json={"stargazers_count": 99},
@@ -13,8 +16,14 @@ def test_prefect_simple_example_flow(prefect_harness, httpx_mock, caplog):
         url="https://api.github.com/repos/EBI-Metagenomics/notebooks",
         json={"stargazers_count": 33},
     )
-    stars = github_stars(
-        ["EBI-Metagenomics/emg-viral-pipeline", "EBI-Metagenomics/notebooks"]
+
+    logger = logging.getLogger("prefect")
+    logger.propagate = True
+
+    stars_flow_run = run_flow_and_capture_logs(
+        github_stars,
+        ["EBI-Metagenomics/emg-viral-pipeline", "EBI-Metagenomics/notebooks"],
     )
-    assert "stars" in caplog.text
-    assert stars == [99, 33]
+
+    assert "stars" in stars_flow_run.logs
+    assert stars_flow_run.result == [99, 33]
