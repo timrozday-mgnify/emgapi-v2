ded650d000591c19726072c5fec642a1bcc98738
sandyr <sandyr@ebi.ac.uk>
2024-08-01 10:57:53 +0100

    adds tests for biomes/hierarchies

--- analyses/tests.py
+++ analyses/tests.py
@@ -1,5 +1,5 @@
 import pytest
-from .models import Study
+from .models import Study, Biome
 from ena.models import Study as ENAStudy
 
 
@@ -9,3 +9,84 @@ def test_study():
     ena_study = ENAStudy.objects.create(accession="PRJ1", title="Project 1")
     study = Study.objects.create(ena_study=ena_study, title="Project 1")
     assert study.accession == "MGYS00000001"
+
+
+def test_biome_lineage_path_generator():
+    assert Biome.lineage_to_path("root") == "root"
+    assert Biome.lineage_to_path("Root") == "root"
+    assert (
+        Biome.lineage_to_path("root:Host-associated:Human:Digestive system")
+        == "root.host_associated.human.digestive_system"
+    )
+    assert (
+        Biome.lineage_to_path("root:Host-associated:Human:Digestive system (bowel)")
+        == "root.host_associated.human.digestive_system_bowel"
+    )
+
+
+@pytest.mark.django_db
+def test_biome_hierarchy():
+    root: Biome = Biome.objects.create(biome_name="root", path="root")
+    engineered: Biome = Biome.objects.create(
+        biome_name="Engineered", path="root.engineered"
+    )
+    wet_ferm: Biome = Biome.objects.create(
+        biome_name="Wet fermentation", path="root.engineered.wet_fermentation"
+    )
+    cont_cult: Biome = Biome.objects.create(
+        biome_name="Continuous culture",
+        path="root.engineered.bioreactor.continuous_culture",
+    )
+
+    # only one root to hierarchy
+    assert Biome.objects.roots().count() == 1
+
+    # root has 4 descendants (includes self)
+    root.refresh_from_db()
+    assert root.descendants_count == 4
+
+    # engineered has 3
+    engineered.refresh_from_db()
+    assert engineered.descendants_count == 3
+
+    # wet fermentaiton has 1 (self)
+    wet_ferm.refresh_from_db()
+    assert wet_ferm.descendants_count == 1
+
+    # pretty lineage is colon separated with capitals and spaces
+    assert wet_ferm.pretty_lineage == "root:Engineered:Wet fermentation"
+
+    # if a level of hierarchy is skipped (bioreactor) it is simply missing in pretty version (known limitation)
+    cont_cult.refresh_from_db()
+    assert cont_cult.pretty_lineage == "root:Engineered:Continuous culture"
+
+    # ancestors are as expected (self, engineered, root)
+    assert cont_cult.ancestors().count() == 3
+    assert cont_cult.ancestors().filter(biome_name="root").exists()
+    assert cont_cult.ancestors().filter(biome_name="Engineered").exists()
+    assert cont_cult.ancestors().filter(biome_name="Continuous culture").exists()
+
+    # (direct) children should be 1
+    assert engineered.children().count() == 1
+    # add another child
+    Biome.objects.create(biome_name="Bioreactor", path="root.engineered.bioreactor")
+    assert engineered.children().count() == 2
+
+
+@pytest.mark.django_db
+def test_study_biome_lookups(top_level_biomes, mgnify_study):
+    cont_cult: Biome = Biome.objects.create(
+        biome_name="Continuous culture",
+        path="root.engineered.bioreactor.continuous_culture",
+    )
+    mgnify_study.biome = cont_cult
+    mgnify_study.save()
+
+    # something like a lineage lookup:
+    eng = Biome.objects.get(path=Biome.lineage_to_path("root:Engineered"))
+
+    # find studies within lineage. this lookup is for studies with a biome whose path is IN the descendants of engineered
+    # (slightly counterintuitive naming)
+    eng_studies = Study.objects.filter(biome__path__descendants=eng.path)
+    assert eng_studies.count() == 1
+    assert eng_studies.first() == mgnify_study
