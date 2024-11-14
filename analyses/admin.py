c100d807b97081c8b74b104606d282e7cd082d9f
Sandy Rogers <sandyr@ebi.ac.uk>
2024-11-14 12:43:25 +0000

    adds some filtering/list display usefulness to assembly admin

--- analyses/admin.py
+++ analyses/admin.py
@@ -119,9 +119,54 @@ class RunAdmin(ModelAdmin):
     pass
 
 
+class AssemblyStatusListFilter(admin.SimpleListFilter):
+    title = "status"
+
+    parameter_name = "status"
+
+    def lookups(self, request, model_admin):
+        return [
+            (state, state.replace("_", " ").title())
+            for state in Assembly.AssemblyStates
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        return queryset.filter(**{f"status__{self.value()}": True})
+
+
 @admin.register(Assembly)
 class AssemblyAdmin(ModelAdmin):
-    pass
+    list_display = [
+        "__str__",
+        "created_at",
+        "updated_at",
+        "ena_study",
+        "status_summary",
+    ]
+    list_filter = ["updated_at", "created_at", AssemblyStatusListFilter]
+    search_fields = [
+        "id",
+        "run__ena_accessions",
+        "ena_study__title",
+        "ena_study__accession",
+        "ena_study__additional_accessions",
+        "assembly_study__accession",
+        "assembly_study__ena_study__accession",
+        "assembly_study__ena_study__additional_accessions",
+        "reads_study__accession",
+        "reads_study__title",
+        "reads_study__ena_study__accession",
+        "reads_study__ena_study__additional_accessions",
+    ]
+
+    def status_summary(self, obj):
+        if (not obj.status) or (not type(obj.status) is dict):
+            return None
+        return " — ".join(
+            [status.upper() for status, is_set in obj.status.items() if is_set]
+        )
 
 
 @admin.register(Analysis)
