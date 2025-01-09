9ecc81d08f463aca06bd98fac2e1d8cd8ec108b6
Sandy Rogers <sandyr@ebi.ac.uk>
2025-01-09 13:35:04 +0000

    switches codon deployment from codon to venv

--- deployment/ebi-wp-k8s-hl/start_mitm_with_monit.sh
+++ deployment/ebi-wp-k8s-hl/start_mitm_with_monit.sh
@@ -4,8 +4,7 @@ MITMPIDFILE=/nfs/production/rdf/metagenomics/jenkins-slurm/processes/dev-mi-slur
 
 source /nfs/production/rdf/metagenomics/jenkins-slurm/dev-prefect-agent/deployment/ebi-wp-k8s-hl/secrets-dev-mi-slurm-worker.env
 
-source /hps/software/users/rdf/metagenomics/service-team/software/miniconda_py39/etc/profile.d/conda.sh
-conda activate dev-prefect-agent
+source /nfs/production/rdf/metagenomics/jenkins-slurm/dev-prefect-agent/venv/bin/activate
 
 cd /nfs/production/rdf/metagenomics/jenkins-slurm/dev-prefect-agent/
 mitmdump --mode upstream:http://hh-wwwcache.ebi.ac.uk:3128 -s deployment/ebi-wp-k8s-hl/mitm_auth_for_workers.py >> /nfs/production/rdf/metagenomics/jenkins-slurm/logs/dev-mi-slurm-mitmproxy.log 2>&1 &
--- deployment/ebi-wp-k8s-hl/start_worker_with_monit.sh
+++ deployment/ebi-wp-k8s-hl/start_worker_with_monit.sh
@@ -4,8 +4,7 @@ DJANGOPIDFILE=/nfs/production/rdf/metagenomics/jenkins-slurm/processes/dev-mi-sl
 
 source /nfs/production/rdf/metagenomics/jenkins-slurm/dev-prefect-agent/deployment/ebi-wp-k8s-hl/secrets-dev-mi-slurm-worker.env
 
-source /hps/software/users/rdf/metagenomics/service-team/software/miniconda_py39/etc/profile.d/conda.sh
-conda activate dev-prefect-agent
+source /nfs/production/rdf/metagenomics/jenkins-slurm/dev-prefect-agent/venv/bin/activate
 
 cd /nfs/production/rdf/metagenomics/jenkins-slurm/dev-prefect-agent/
 env HTTP_PROXY="http://127.0.0.1:8080" python manage.py prefectcli worker start --pool "slurm" >> /nfs/production/rdf/metagenomics/jenkins-slurm/logs/dev-mi-slurm-worker-prefect-worker.log 2>&1 &
