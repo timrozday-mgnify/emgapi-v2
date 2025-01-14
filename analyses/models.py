7146344ff92c28dbf8e64b027964c311fd694849
Mahfouz Shehu <mahfouzshehu@gmail.com>
2025-01-14 13:28:16 +0000

    Update analyses/models.py
    Co-authored-by: Sandy Rogers <sandyr@ebi.ac.uk>

--- analyses/models.py
+++ analyses/models.py
@@ -432,7 +432,7 @@ class PublicAnalysisManager(
     pass
 
 
-class PublicAnalysisManagerMixinIncludingAnnotations(
+class PublicAnalysisManagerIncludingAnnotations(
     PrivacyFilterManagerMixin, AnalysisManagerIncludingAnnotations
 ):
     """
