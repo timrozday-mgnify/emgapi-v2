50ff2486cce9a9c57c7afddafe8e256d3d333125
MGS-sails <mahfouzshehu@gmail.com>
2025-01-14 14:04:38 +0000

    Added auth decorator to my-data/studies endpoint as well

--- emgapiv2/api.py
+++ emgapiv2/api.py
@@ -253,7 +253,7 @@ def list_mgnify_analyses(request):
     operation_id="list_private_mgnify_analyses",
     auth=django_auth_superuser,
 )
-def list_private_mgnify_analyses(request):
+def list_private_mgnify_analyses():
     qs = analyses.models.Analysis.objects.private_only()
     return qs
 
@@ -264,8 +264,9 @@ def list_private_mgnify_analyses(request):
     tags=[ApiSections.STUDIES.value],
     summary="List all private studies available from MGnify",
     operation_id="list_private_mgnify_studies",
+    auth=django_auth_superuser,
 )
-def list_private_mgnify_studies(request):
+def list_private_mgnify_studies():
     qs = analyses.models.Study.objects.private_only()
     return qs
 
