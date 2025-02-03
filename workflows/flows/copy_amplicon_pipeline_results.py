7586836babee89cd0bc6e2c0ab0150e4863f84b6
MGS-sails <mahfouzshehu@gmail.com>
2025-02-03 18:43:59 +0000

    Made results source and target dirs dyunamic

--- workflows/flows/copy_amplicon_pipeline_results.py
+++ workflows/flows/copy_amplicon_pipeline_results.py
@@ -1,5 +1,9 @@
 from prefect import flow
+import django
+django.setup()
 
+from analyses.models import Analysis
+from workflows.data_io_utils.mgnify_v6_utils.amplicon import EMG_CONFIG
 from workflows.prefect_utils.slurm_flow import move_data
 
 
@@ -7,9 +11,9 @@ from workflows.prefect_utils.slurm_flow import move_data
     name="Copy Amplicon Pipeline Results",
     flow_run_name="Copy Amplicon Pipeline Results",
 )
-async def copy_amplicon_pipeline_results():
+async def copy_amplicon_pipeline_results(analysis: Analysis):
     await move_data(
-        "/nfs/production/rdf/metagenomics/results",
-        "/nfs/ftp/public/databases/metagenomics/mgnify_results",
+        analysis.results_dir,
+        f"{EMG_CONFIG.slurm.ftp_results_dir}/{analysis.study.first_accession[:4]}/{analysis.study.first_accession}",
         "cp -r",
     )
