5b1a45d61a76bdf8f24ba974ea5ceca7b3412f38
Sandy Rogers <sandyr@ebi.ac.uk>
2025-03-25 13:36:20 +0000

    workaround for another TSV formatting edge case

--- workflows/data_io_utils/file_rules/rule_factories.py
+++ workflows/data_io_utils/file_rules/rule_factories.py
@@ -15,12 +15,14 @@ def generate_csv_schema_file_rule(
     row_schema: Type[BaseModel],
     delimiter: CSVDelimiter = CSVDelimiter.COMMA,
     none_values: Iterable[Hashable] = None,
+    allow_trailing_delimiters: bool = True,
 ) -> FileRule:
     """
     Generate a FileRule in which the rule test checks if the CSV file at path follows the specified schema.
     :param row_schema: A Pydantic model to validate each CSV row against
     :param delimiter: Field separator for CSV file. E.g. CSVDelimiter.COMMA
     :param none_values: Optional list of values that should be parsed to None, e.g. ["", "NA"]
+    :param allow_trailing_delimiters: Whether to allow (ignore) trailing delimiters after the final dataful column
     :return: A FileRule that can be applied to any path
     """
     schema_name = row_schema.__name__
@@ -33,6 +35,17 @@ def generate_csv_schema_file_rule(
             rows_count = 0
             try:
                 for row in reader:
+                    if allow_trailing_delimiters:
+                        # sometimes TSVs may have a trailing tab on data rows.
+                        # it is probably strictly invalid, but sometimes seen.
+                        if None in row and row[None] in [
+                            "",
+                            delimiter,
+                            [],
+                            [""],
+                            [delimiter],
+                        ]:
+                            row.pop(None, None)
                     row_schema.model_validate(row)
                     rows_count += 1
             except ValidationError as e:
